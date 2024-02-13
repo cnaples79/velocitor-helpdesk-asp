6707a98c0a3214f369b083eb953c804d84d0954c
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-02-13 16:26:11 -0500

    Updates

--- scripts/site.ts
+++ scripts/site.ts
@@ -1,2 +1,20 @@
-﻿const greeting: string = "Hello world!";
-console.log(greeting);
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const sidebarToggle = document.getElementById('sidebar-toggle') as HTMLInputElement;
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+function handleSidebarToggle(mediaQuery) {
+  if (mediaQuery.matches) {
+    sidebarToggle.checked = true;
+  } else {
+    sidebarToggle.checked = false;
+  }
+}
+
+// Initial check on page load
+handleSidebarToggle(mediaQuery);
+
+// Listen for screen resize
+mediaQuery.addEventListener('change', handleSidebarToggle);
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,18 @@
-var greeting = "Hello world!";
-console.log(greeting);
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+// Write your JavaScript code.
+var sidebarToggle = document.getElementById('sidebar-toggle');
+var mediaQuery = window.matchMedia('(min-width: 768px)');
+function handleSidebarToggle(mediaQuery) {
+    if (mediaQuery.matches) {
+        sidebarToggle.checked = true;
+    }
+    else {
+        sidebarToggle.checked = false;
+    }
+}
+// Initial check on page load
+handleSidebarToggle(mediaQuery);
+// Listen for screen resize
+mediaQuery.addEventListener('change', handleSidebarToggle);
 //# sourceMappingURL=site.js.map
